40062b5dda6886d887ff03e0362c1994d23bfccc
KavyaSunithaVijayan <kavyavijayan6713@gmail.com>
2025-06-29 20:32:26 +0530

    Feature: UI added

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -8,36 +8,35 @@ function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header>
-      <div className="p-5 flex items-center justify-between bg-[#B5BDE1]">
+    <header className="bg-blue-100">
+      <div className="p-5 flex items-center justify-between">
         <div className="flex gap-25">
-          <h4 className="text-2xl poppins-semibold text-white">
+          {/* <h4 className="text-2xl poppins-semibold text-white">
             Kavya Vijayan
-          </h4>
+          </h4> */}
         </div>
 
         <div className="flex items-center gap-4">
-          <h1 className="text-white hover:text-blue-500 text-2xl">
+          <h1 className="text-black hover:text-blue-500 text-2xl">
             <a href="https://www.linkedin.com/in/kavya-vijayan-a89b3525b/">
               <TiSocialLinkedin />
             </a>
           </h1>
-          <h1 className="text-white hover:text-green-500 text-2xl">
+          <h1 className="text-black hover:text-green-500 text-2xl">
             <a href="https://github.com/KavyaSunithaVijayan">
               <FaGithub />
             </a>
           </h1>
-          <div className="text-white">
+          <div className="text-black">
             <RxHamburgerMenu onClick={() => setShowMenu(!showMenu)} />
           </div>
         </div>
       </div>
       <div
-        className={`fixed top-0 z-50 bottom-0 w-[50%] h-screen bg-[#E5DDF4] transition-transform duration-300 ease-linear  ${
-          showMenu
-            ? "translate-x-0 right-0"
-            : "translate-x-[100%] -right-[160px]"
-        }`}
+        className={`fixed top-0 z-50 bottom-0 w-[50%] h-screen bg-[#E5DDF4] transition-transform duration-300 ease-linear  ${showMenu
+          ? "translate-x-0 right-0"
+          : "translate-x-[100%] -right-[160px]"
+          }`}
       >
         <button onClick={() => setShowMenu(!showMenu)} className="p-0 ">
           <IoIosClose size="30px" />
@@ -51,17 +50,17 @@ function Header() {
           <li className="text-violet-800 poppins-semibold pb-3">
             <a href="#home">HOME</a>
           </li>
-          <li className="text-white hover:text-violet-900 poppins-light poppins-semibold pb-3">
+          <li className="text-black hover:text-violet-900 poppins-light poppins-semibold pb-3">
             <a href="#about"> ABOUT</a>
           </li>
-          <li className="text-white hover:text-violet-900 poppins-light poppins-semibold pb-3">
+          <li className="text-black hover:text-violet-900 poppins-light poppins-semibold pb-3">
             <a href="#whatIDo">WHAT I DO</a>
           </li>
-          <li className="text-white hover:text-violet-900 poppins-light poppins-semibold pb-3">
+          <li className="text-black hover:text-violet-900 poppins-light poppins-semibold pb-3">
             <a href="#work">WORK</a>
           </li>
 
-          <li className="text-white hover:text-violet-900 poppins-light poppins-semibold pb-3">
+          <li className="text-black hover:text-violet-900 poppins-light poppins-semibold pb-3">
             <a href="#contact">CONTACT</a>
           </li>
         </ul>
--- src/pages/portfolio/homePage/index.tsx
+++ src/pages/portfolio/homePage/index.tsx
@@ -1,10 +1,11 @@
 import Animation from "../../../components/content/animationObserver";
+import '../../../App.css';
 
 function HomePage() {
   return (
     <div>
-      <div className="max-w-screen-xl mx-auto bg-white grid sm:grid-cols-1 md:grid-cols-2 gap-3 sm:gap-5 items-center py-16 ">
-        <div id="home" className="order-2 lg:order-1">
+      <div className="max-w-screen-xl mx-auto bg-white grid sm:grid-cols-1 md:grid-cols-2 gap-3 sm:gap-5 pb-16 items-center ">
+        {/* <div id="home" className="order-2 lg:order-1">
           <h4 className=" text-violet-950 poppins-bold text-4xl lg:text-6xl pb-10 ">
             I'm Kavya Vijayan
           </h4>
@@ -23,16 +24,30 @@ function HomePage() {
               </button>
             </a>
           </div>
+        </div> */}
+        {/* <Animation animationType={"animate__slideInRight "} className="order-2 lg:order-1"> */}
+
+        {/* </Animation> */}
+        <div className="relative">
+          <h2 className="text-9xl tracking-widest text-violet-100 font-extrabold"> KAVYA</h2>
+          <div className="absolute -top-5 left-30">
+            <div className="flex items-center gap-2">
+              <h2 className="text-5xl font-semibold tracking-wider">HELLO</h2>
+              <hr className="text-black w-60 font-bold" />
+            </div>
+            <h2 className="py-5 text-5xl font-semibold tracking-wider">I AM KAVYA VIJAYAN</h2>
+            <span className="text-2xl animative-text !overflow-hidden !whitespace-nowrap !border-r">REACT DEVELOPER</span>
+
+
+          </div>
         </div>
-        <Animation animationType={"animate__slideInRight "} className="order-2 lg:order-1">
-          <img
-            src="/image/working_girl.jpg"
-            alt="working_girl"
-            className="w-96 lg:w-full"
-          />
-        </Animation>
+        <img
+          src="/image/working_girl.jpg"
+          alt="working_girl"
+          className="w-96 lg:w-full"
+        />
       </div>
-    </div>
+    </div >
   );
 }
 
